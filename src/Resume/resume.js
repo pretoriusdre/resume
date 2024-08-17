f5ffaf524af9a56217181da2294306e8944f64e1
Andre <pretorius.dre+github@gmail.com>
2024-08-17 23:43:14 +0800

    Remove errant symbol

--- src/Resume/resume.js
+++ src/Resume/resume.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState }  from 'react';
 import { DndProvider} from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-=
+
 import './Resume.css';
 
 
