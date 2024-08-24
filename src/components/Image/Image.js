feef269f2cd8e24d3296df312b8abaebfa2d0a3e
Andre <pretorius.dre+github@gmail.com>
2024-08-24 14:41:25 +0800

    Image modal include background

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,18 +2,18 @@
 
 import './Image.css'
 
-const Image = ({title, src, collapsed, toggleCollapse}) => {
-
-
-    const modalClass = `imgmodal${collapsed ? ' hidden' : ''}`
+const Image = ({ title, src, collapsed, toggleCollapse }) => {
+    const modalClass = `imgmodal${collapsed ? ' hidden' : ''}`;
+    const overlayClass = `overlay${collapsed ? ' hidden' : ''}`;
 
     return (
         <div>
             <img src={`${process.env.PUBLIC_URL}${src}`} className="imgtiny" onClick={toggleCollapse} title={title} alt={title} />
-            <img src={`${process.env.PUBLIC_URL}${src}`} className={modalClass} onClick={toggleCollapse} title={title} alt={title} />
+            <div className={overlayClass} onClick={toggleCollapse}>
+                <img src={`${process.env.PUBLIC_URL}${src}`} className={modalClass} title={title} alt={title} />
+            </div>
         </div>
     );
-    
 };
 
 export default Image;
